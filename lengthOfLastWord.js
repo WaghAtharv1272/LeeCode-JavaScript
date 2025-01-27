6e0c55f21a65e4db6c58da4ae1548a9122c8e6ae
WaghAtharv1272 <atharvwagh1272@gmail.com>
2025-01-27 15:36:13 +0530

    Create lengthOfLastWord.js

---
+++ lengthOfLastWord.js
@@ -0,0 +1,18 @@
+/*Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal 
+substring
+ consisting of non-space characters only.
+ */
+ function lengthOfLastWord(string){
+
+
+    let words=string.trim().split(" ");
+
+    return words[words.length-1].length;
+
+}
+
+let string ="Hello World";
+var ans = lengthOfLastWord(string);
+console.log(ans)
